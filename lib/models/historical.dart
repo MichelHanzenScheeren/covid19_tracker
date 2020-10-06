9a1ad49348f09fc056afc5cf01fdba2c12b6f602
MichelHanzenScheeren <michelhanzenscheeren@gmail.com>
2020-10-05 22:58:57 -0300

    Adição de verificação a factory da classe Historical para garantir que funcione para os 2 modelos possíveis de retorno.

--- lib/models/historical.dart
+++ lib/models/historical.dart
@@ -6,6 +6,7 @@ class Historical {
   List<HistoricalItem> recovered;
 
   Historical.fromMap(Map map) {
+    if (map.containsKey('timeline')) map = map['timeline'];
     cases = _getItems(map['cases']);
     deaths = _getItems(map['deaths']);
     recovered = _getItems(map['recovered']);
--- test/models_test/historical_test.dart
+++ test/models_test/historical_test.dart
@@ -8,15 +8,40 @@ const Map model = {
   "recovered": {"10/3/20": 24146918, "10/4/20": 24363259}
 };
 
+const Map model2 = {
+  "country": "Brazil",
+  "province": ["mainland"],
+  "timeline": {
+    "cases": {"10/3/20": 4906833, "10/4/20": 4915289},
+    "deaths": {"10/3/20": 145987, "10/4/20": 146352},
+    "recovered": {"10/3/20": 4361108, "10/4/20": 4375354}
+  }
+};
+
 void main() {
-  test('Validação da factory Historical.fromMap(Map)', () {
-    Historical historical = Historical.fromMap(model);
-    expect(historical, isInstanceOf<Historical>());
-    expect(historical.cases, isInstanceOf<List<HistoricalItem>>());
-    expect(historical.deaths, isInstanceOf<List<HistoricalItem>>());
-    expect(historical.recovered, isInstanceOf<List<HistoricalItem>>());
-    expect(historical.cases.length, equals(2));
-    expect(historical.deaths.length, equals(2));
-    expect(historical.recovered.length, equals(2));
-  });
+  group(
+    'Validação de Historical.fromMap(Map) nos 2 casos possíveis de retorno',
+    () {
+      test('Validação de Historical.fromMap(Map) model1', () {
+        Historical historical = Historical.fromMap(model);
+        expect(historical, isInstanceOf<Historical>());
+        expect(historical.cases, isInstanceOf<List<HistoricalItem>>());
+        expect(historical.deaths, isInstanceOf<List<HistoricalItem>>());
+        expect(historical.recovered, isInstanceOf<List<HistoricalItem>>());
+        expect(historical.cases.length, equals(2));
+        expect(historical.deaths.length, equals(2));
+        expect(historical.recovered.length, equals(2));
+      });
+      test('Validação da Historical.fromMap(Map) model2', () {
+        Historical historical = Historical.fromMap(model2);
+        expect(historical, isInstanceOf<Historical>());
+        expect(historical.cases, isInstanceOf<List<HistoricalItem>>());
+        expect(historical.deaths, isInstanceOf<List<HistoricalItem>>());
+        expect(historical.recovered, isInstanceOf<List<HistoricalItem>>());
+        expect(historical.cases.length, equals(2));
+        expect(historical.deaths.length, equals(2));
+        expect(historical.recovered.length, equals(2));
+      });
+    },
+  );
 }

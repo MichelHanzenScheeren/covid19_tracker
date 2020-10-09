12a3831af0d82077f1dea39a7a1e8e609cc89735
MichelHanzenScheeren <michelhanzenscheeren@gmail.com>
2020-10-09 00:40:01 -0300

    Criação da classe SQFliteService, responsável por conectar-se e criar o banco de dados; Criação da classe SQFliteError, para tratar possíveis erros relacionados ao banco.

---
+++ lib/data/services/sqflite_service.dart
@@ -0,0 +1,47 @@
+import 'package:covid19_tracker_in_flutter/data/errors/sqflite_error.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+const String DATABASE_NAME = 'covid19_tracker_in_flutter.db';
+const int DATABASE_VERSION = 1;
+
+class SQFliteService {
+  Database _database;
+  String _databasePath;
+
+  Future<Database> getDatabase() async {
+    if (_database == null) _database = await _openDatabase();
+    return _database;
+  }
+
+  Future<Database> _openDatabase() async {
+    try {
+      return await openDatabase(
+        await _getPath(),
+        version: DATABASE_VERSION,
+        onCreate: _createTables,
+      );
+    } catch (error) {
+      if (error is SQFliteError) throw error;
+      throw SQFliteError(type: SQFliteErrorType.open);
+    }
+  }
+
+  Future<String> _getPath() async {
+    try {
+      if (_databasePath == null)
+        _databasePath = join(await getDatabasesPath(), DATABASE_NAME);
+      return _databasePath;
+    } catch (error) {
+      throw SQFliteError(type: SQFliteErrorType.getPath);
+    }
+  }
+
+  Future _createTables(Database db, int version) async {
+    try {
+      await db.execute('');
+    } catch (error) {
+      throw SQFliteError(type: SQFliteErrorType.create);
+    }
+  }
+}
